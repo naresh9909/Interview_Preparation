38df89cc35bae8145a9476056a52a6b171af3efc
naresh9909 <b.ballampalli.naresh@gmail.com>
2023-08-24 23:09:53 -0600

    Notes

---
+++ src/Notes.java
@@ -0,0 +1,20 @@
+
+public class Notes {
+	/*
+	 * Final - Variable, Methods, Class
+	 * Read date from CSV file
+	 * Read date from EXCEL file
+	 * OOPs Concepts and Examples
+	 * Question and Answers
+	 * Java8
+	 * 
+	 * 
+	 * 
+	 * API Automation Framework
+	 * Automate GET or PUT or POST
+	 * JSON finder method
+	 * 
+	 * 
+	 */
+
+}
