a911213b6709920bceda99642d311fb67e87af03
naresh9909 <b.ballampalli.naresh@gmail.com>
2023-08-26 19:58:31 -0600

    Reading files

---
+++ src/ENUM_Package/enumEnum.java
@@ -0,0 +1,7 @@
+package ENUM_Package;
+
+public enum enumEnum {
+	
+	SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+
+}
---
+++ src/ENUM_Package/enumRunner.java
@@ -0,0 +1,10 @@
+package ENUM_Package;
+
+public class enumRunner {
+
+	public static void main(String[] args) {
+		System.out.println(enumEnum.FRIDAY);
+
+	}
+
+}
