8c38f7b3568c54bf1c27d73dad4a56a29270a4d5
naresh9909 <b.ballampalli.naresh@gmail.com>
2023-10-12 16:02:35 -0600

    Added Assertions

--- src/test/Feature1.java
+++ src/test/Feature1.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,16 +15,19 @@ public class Feature1 {
 	@AfterClass
 	public void EndStep() {
 		System.out.println("This is @AfterClass: Feature1 class");
+		Assert.assertEquals(true, true);
 	}
 	
 	@Test
 	public void F1_Test1() {
 		System.out.println("Test case F1_Test1");
+		Assert.assertEquals(false, false);
 	}
 	
 	@Test
 	public void F1_Test2() {
 		System.out.println("Test case F1_Test2");
+		Assert.assertEquals(true, false);
 	}
 	
 	@Test
